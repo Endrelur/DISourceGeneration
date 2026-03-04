4e4d0a870d65371bb45c86b0767e740a4ce77221
EndreLur <endre.had@gmail.com>
2026-03-04 21:44:14 +0100

    add GeneratedObjectTemplates.cs

---
+++ src/DiSourceGenerator/Helpers/GeneratedObjectTemplates.cs
@@ -0,0 +1,24 @@
+﻿namespace DiSourceGenerator.Helpers;
+
+internal static class GeneratedObjectTemplates
+{
+    public static string ServiceCollection(string name, string? @namespace)
+    {
+        var actualNamespace = string.IsNullOrWhiteSpace(@namespace) ? string.Empty : $"namespace {@namespace};";
+
+    return $@"
+using Microsoft.Extensions.DependencyInjection;
+
+{actualNamespace}
+
+static partial class {name}
+{{
+    public static IServiceCollection Register{name}Services(this IServiceCollection services)
+    {{
+        // Your generation logic for service registration goes here
+        return services;
+    }}
+}}
+";
+}
+    }

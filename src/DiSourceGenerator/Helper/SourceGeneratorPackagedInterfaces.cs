6efa52de9e3aa8fb29490adb8d194720a48e6f14
EndreLur <endre.had@gmail.com>
2026-03-04 20:27:43 +0100

    add interface IServiceModule

---
+++ src/DiSourceGenerator/Helper/SourceGeneratorPackagedInterfaces.cs
@@ -0,0 +1,15 @@
+﻿namespace DiSourceGenerator.Helper;
+
+internal static class SourceGeneratorPackagedInterfaces
+{
+    public const string IServiceInterface = $@"
+using Microsoft.Extensions.DependencyInjection;
+
+{SourceGeneratorPackagedNamespaces.AutoDiRegistrationAttributes};
+
+public interface IServiceModule
+{{     
+    public IServiceCollection AddServicesToServiceCollection(IServiceCollection serviceCollection);
+}} 
+";
+}
